e632559dbc4b76b5135b6b20ec13b44a8ff921f9
aaditya9803 <aaditya9803@gmail.com>
2025-01-23 18:28:38 +0100

    back to initial version

--- actions/actions.py
+++ actions/actions.py
@@ -1,35 +1,27 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# This is a simple example for a custom action which utters "Hello World!"
 
-
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
 # class ActionHelloWorld(Action):
-
+#
 #     def name(self) -> Text:
 #         return "action_hello_world"
-
+#
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+#
 #         dispatcher.utter_message(text="Hello World!")
-
+#
 #         return []
-
-class Extract_symptopms(Action):
-
-    def name(self) -> Text:
-        return "action_symptoms_entity"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        symptom = next(tracker.get_latest_entity_values('disease'), None)
-
-        if symptom:
-            dispatcher.utter_message(text="I see you have a symptom of {symptom}".format(symptom))
-        else:
-            dispatcher.utter_message(text="I could not find any symptom")
-        return []
