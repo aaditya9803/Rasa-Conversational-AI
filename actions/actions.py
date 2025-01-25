4a89dc39309a2e8eb198dc36e3a9ff29e3e1104e
aaditya9803 <aaditya9803@gmail.com>
2025-01-25 05:19:19 +0100

    added question for age height weight

--- actions/actions.py
+++ actions/actions.py
@@ -1,45 +1,67 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-#
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
 
-class ActionHelloWorld(Action):
+class ActionHandleDirectInput(Action):
 
-    def name(self):
-        return "action_extract_age"
+    def name(self) -> Text:
+        return "action_handle_direct_input"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         age = tracker.get_slot('age')
+        height = tracker.get_slot('height')
+        weight = tracker.get_slot('weight')
 
-        if age:
-            # Convert age to integer
-            try:
-                age = int(age)
-                return [SlotSet("extracted_age", age)]
-            except ValueError:
-                return [SlotSet("extracted_age", None)]
-
-        # dispatcher.utter_message(text="Hello World!")
+        if age and not height:
+            dispatcher.utter_message(response="utter_ask_height")
+        elif height and not weight:
+            dispatcher.utter_message(response="utter_ask_weight")
+        elif weight and not age:
+            dispatcher.utter_message(response="utter_greet")
+        else:
+            dispatcher.utter_message(response="utter_direct_input")
 
         return []
-    
-    # def extract_age(self, dispatcher, tracker, domain):
-    #     age_entity = next(tracker.get_latest_entity_values('age'), None)
-    #     if age_entity:
-    #         print(age_entity)
-    #         return int(age_entity)
-    #     return None
+
+
+class ActionCalculateBMI(Action):
+
+    def name(self) -> Text:
+        return "action_calculate_bmi"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        height = tracker.get_slot('height')
+        weight = tracker.get_slot('weight')
+
+        if height and weight:
+            try:
+                height = float(height)
+                weight = float(weight)
+                bmi = weight / (height ** 2)
+                bmi_category = self.get_bmi_category(bmi)
+                dispatcher.utter_message(response="utter_bmi_result", bmi=bmi, bmi_category=bmi_category)
+                return [SlotSet("bmi", bmi), SlotSet("bmi_category", bmi_category)]
+            except ValueError:
+                dispatcher.utter_message(text="Sorry, I couldn't calculate your BMI. Please provide valid height and weight.")
+                return []
+        else:
+            dispatcher.utter_message(text="I need both your height and weight to calculate your BMI.")
+            return []
+
+    def get_bmi_category(self, bmi: float) -> Text:
+        if bmi < 18.5:
+            return "underweight"
+        elif 18.5 <= bmi < 25:
+            return "normal weight"
+        elif 25 <= bmi < 30:
+            return "overweight"
+        else:
+            return "obese"
